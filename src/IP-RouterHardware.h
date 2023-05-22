485942a2255e483b5bfbdfaa63a3659c6995dea2
Ing-Dom <dom@ing-dom.de>
2023-05-22 17:11:55 +0200

    PIPICO Pin information added

--- src/IP-RouterHardware.h
+++ src/IP-RouterHardware.h
@@ -23,6 +23,16 @@
 
 #define HARDWARE_NAME "PIPICO"
 
+#define PROG_LED_PIN LED_BUILTIN
+#define PROG_LED_PIN_ACTIVE_ON HIGH
+#define PROG_BUTTON_PIN 7
+#define PROG_BUTTON_PIN_INTERRUPT_ON FALLING
+#define SAVE_INTERRUPT_PIN 6
+#define INFO_LED_PIN 3
+#define INFO_LED_PIN_ACTIVE_ON HIGH
+#define KNX_UART_RX_PIN 1
+#define KNX_UART_TX_PIN 0
+
 // Definition for PiPico / SPI0
 //#define PIN_MISO_ (16)
 //#define PIN_MOSI_ (19)
@@ -30,10 +40,10 @@
 //#define PIN_SS_ (17)
 
 // Definition for PiPico / SPI1 (default)
-//#define PIN_MISO_ (12)
-//#define PIN_MOSI_ (15)
-//#define PIN_SCK_ (14)
-//#define PIN_SS_ (13)
+#define PIN_MISO_ (12)
+#define PIN_MOSI_ (15)
+#define PIN_SCK_ (14)
+#define PIN_SS_ (13)
 
 // Definition for PiPico / SPI1
 //#define PIN_MISO_ (12)
@@ -41,12 +51,9 @@
 //#define PIN_SCK_ (10)
 //#define PIN_SS_ (13)
 
-#define PROG_BUTTON_PIN 8
-#define PROG_LED_PIN LED_BUILTIN
-#define PROG_LED_PIN_ACTIVE_ON HIGH
 
-//#define PIN_SD_SS (16)
-//#define PIN_ETH_INT (17)
-//#define PIN_ETH_RES (18)
+#define PIN_SD_SS (9)
+#define PIN_ETH_INT (10)
+#define PIN_ETH_RES (11)
 
 #endif

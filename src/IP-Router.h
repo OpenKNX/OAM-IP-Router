10c49160d2cc4f7514081aa0bbf4ca84a6d4cbaa
Ing-Dom <dom@ing-dom.de>
2023-05-31 00:15:56 +0200

    minimal xml ready to use

--- src/IP-Router.h
+++ src/IP-Router.h
@@ -8,10 +8,12 @@
             (time & 0xC000) == 0x8000 ? ((time & 0x3FFF) > 1000 ? 3600000 : \
                                          (time & 0x3FFF) * 3600000 ) : 0 )
                                              
-// Parameter with single occurrence
-
-
 #define MAIN_OpenKnxId 0xA1
 #define MAIN_ApplicationNumber 30
 #define MAIN_ApplicationVersion 1
+#define MAIN_ParameterSize -1
+#define MAIN_MaxKoNumber 0
 #define MAIN_OrderNumber "SYS-REG1-IP-Router"
+// Parameter with single occurrence
+
+

5fcd594e89c8bbd2b8109dba4cb0f485e7328806
Ing-Dom <dom@ing-dom.de>
2023-06-01 07:21:21 +0200

    wip

--- src/IP-Router.h
+++ src/IP-Router.h
@@ -10,10 +10,13 @@
                                              
 #define MAIN_OpenKnxId 0xA1
 #define MAIN_ApplicationNumber 30
-#define MAIN_ApplicationVersion 1
-#define MAIN_ParameterSize -1
+#define MAIN_ApplicationVersion 3
+#define MAIN_ParameterSize 0
 #define MAIN_MaxKoNumber 0
 #define MAIN_OrderNumber "SYS-REG1-IP-Router"
 // Parameter with single occurrence
+#define MultiCast                  0      // 32_t
 
+// MultiCast
+#define ParamMultiCast                 ()
 

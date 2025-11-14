07402b59e911b909fd7cce04e0903865d3dcf2b9
Ing-Dom <dom@ing-dom.de>
2025-11-14 09:26:51 +0100

    wip

--- include/knxprod.h
+++ include/knxprod.h
@@ -15,9 +15,9 @@
 #define MAIN_FirmwareName "IP-Router (Dev)"
 #define MAIN_OpenKnxId 0xA1
 #define MAIN_ApplicationNumber 30
-#define MAIN_ApplicationVersion 94
+#define MAIN_ApplicationVersion 95
 #define MAIN_ApplicationEncoding iso-8859-15
-#define MAIN_ParameterSize 194
+#define MAIN_ParameterSize 188
 #define MAIN_MaxKoNumber 0
 #define MAIN_OrderNumber "OpenKnxIPRouter"
 #define BASE_ModuleVersion 22
@@ -34,22 +34,22 @@
 #define BASE_Watchdog                             5      // 1 Bit, Bit 6
 #define     BASE_WatchdogMask 0x40
 #define     BASE_WatchdogShift 6
-#define BASE_Info1LedFunc                         6      // 32 Bits, Bit 31-0
-#define BASE_Info2LedFunc                        10      // 32 Bits, Bit 31-0
-#define BASE_Info3LedFunc                        14      // 32 Bits, Bit 31-0
-#define BASE_Timezone                            18      // 5 Bits, Bit 7-3
+#define BASE_Info1LedFunc                         6      // 16 Bits, Bit 15-0
+#define BASE_Info2LedFunc                         8      // 16 Bits, Bit 15-0
+#define BASE_Info3LedFunc                        10      // 16 Bits, Bit 15-0
+#define BASE_Timezone                            12      // 5 Bits, Bit 7-3
 #define     BASE_TimezoneMask 0xF8
 #define     BASE_TimezoneShift 3
-#define BASE_CombinedTimeDate                    18      // 1 Bit, Bit 2
+#define BASE_CombinedTimeDate                    12      // 1 Bit, Bit 2
 #define     BASE_CombinedTimeDateMask 0x04
 #define     BASE_CombinedTimeDateShift 2
-#define BASE_SummertimeAll                       18      // 2 Bits, Bit 1-0
+#define BASE_SummertimeAll                       12      // 2 Bits, Bit 1-0
 #define     BASE_SummertimeAllMask 0x03
 #define     BASE_SummertimeAllShift 0
-#define BASE_InternalTime                        18      // 1 Bit, Bit 0
+#define BASE_InternalTime                        12      // 1 Bit, Bit 0
 #define     BASE_InternalTimeMask 0x01
 #define     BASE_InternalTimeShift 0
-#define BASE_TimezoneCustom                      19      // char*, 63 Byte
+#define BASE_TimezoneCustom                      13      // char*, 63 Byte
 
 // Zeitbasis
 #define ParamBASE_StartupDelayBase                    ((knx.paramByte(BASE_StartupDelayBase) & BASE_StartupDelayBaseMask) >> BASE_StartupDelayBaseShift)
@@ -60,11 +60,11 @@
 // Watchdog aktivieren
 #define ParamBASE_Watchdog                            ((bool)(knx.paramByte(BASE_Watchdog) & BASE_WatchdogMask))
 // Info1 (N/A)
-#define ParamBASE_Info1LedFunc                        (knx.paramInt(BASE_Info1LedFunc))
+#define ParamBASE_Info1LedFunc                        (knx.paramWord(BASE_Info1LedFunc))
 // Info2 (IP)
-#define ParamBASE_Info2LedFunc                        (knx.paramInt(BASE_Info2LedFunc))
+#define ParamBASE_Info2LedFunc                        (knx.paramWord(BASE_Info2LedFunc))
 // Info3 (KNX)
-#define ParamBASE_Info3LedFunc                        (knx.paramInt(BASE_Info3LedFunc))
+#define ParamBASE_Info3LedFunc                        (knx.paramWord(BASE_Info3LedFunc))
 // Zeitzone
 #define ParamBASE_Timezone                            ((knx.paramByte(BASE_Timezone) & BASE_TimezoneMask) >> BASE_TimezoneShift)
 // Empfangen über
@@ -76,33 +76,33 @@
 // POSIX TZ-String
 #define ParamBASE_TimezoneCustom                      (knx.paramData(BASE_TimezoneCustom))
 
-#define NET_HostAddress                         83      // IP address, 4 Byte
-#define NET_SubnetMask                          87      // IP address, 4 Byte
-#define NET_GatewayAddress                      91      // IP address, 4 Byte
-#define NET_NameserverAddress                   95      // IP address, 4 Byte
-#define NET_CustomHostname                      99      // 1 Bit, Bit 7
+#define NET_HostAddress                         77      // IP address, 4 Byte
+#define NET_SubnetMask                          81      // IP address, 4 Byte
+#define NET_GatewayAddress                      85      // IP address, 4 Byte
+#define NET_NameserverAddress                   89      // IP address, 4 Byte
+#define NET_CustomHostname                      93      // 1 Bit, Bit 7
 #define     NET_CustomHostnameMask 0x80
 #define     NET_CustomHostnameShift 7
-#define NET_StaticIP                            99      // 1 Bit, Bit 6
+#define NET_StaticIP                            93      // 1 Bit, Bit 6
 #define     NET_StaticIPMask 0x40
 #define     NET_StaticIPShift 6
-#define NET_mDNS                                100      // 1 Bit, Bit 7
+#define NET_mDNS                                94      // 1 Bit, Bit 7
 #define     NET_mDNSMask 0x80
 #define     NET_mDNSShift 7
-#define NET_HTTP                                100      // 1 Bit, Bit 6
+#define NET_HTTP                                94      // 1 Bit, Bit 6
 #define     NET_HTTPMask 0x40
 #define     NET_HTTPShift 6
-#define NET_NTP                                 100      // 1 Bit, Bit 5
+#define NET_NTP                                 94      // 1 Bit, Bit 5
 #define     NET_NTPMask 0x20
 #define     NET_NTPShift 5
-#define NET_OTAUpdate                           100      // 2 Bits, Bit 4-3
+#define NET_OTAUpdate                           94      // 2 Bits, Bit 4-3
 #define     NET_OTAUpdateMask 0x18
 #define     NET_OTAUpdateShift 3
-#define NET_HostName                            101      // char*, 24 Byte
-#define NET_LanMode                             142      // 4 Bits, Bit 7-4
+#define NET_HostName                            95      // char*, 24 Byte
+#define NET_LanMode                             136      // 4 Bits, Bit 7-4
 #define     NET_LanModeMask 0xF0
 #define     NET_LanModeShift 4
-#define NET_NTPServer                           143      // char*, 50 Byte
+#define NET_NTPServer                           137      // char*, 50 Byte
 
 // IP-Adresse
 #define ParamNET_HostAddress                         (knx.paramInt(NET_HostAddress))
@@ -228,7 +228,7 @@
 #define BASE_KommentarModuleModuleParamSize 0
 #define BASE_KommentarModuleSubmodulesParamSize 0
 #define BASE_KommentarModuleParamSize 0
-#define BASE_KommentarModuleParamOffset 194
+#define BASE_KommentarModuleParamOffset 188
 #define BASE_KommentarModuleCalcIndex(index, m1) (index + BASE_KommentarModuleParamOffset + _channelIndex * BASE_KommentarModuleCount * BASE_KommentarModuleParamSize + m1 * BASE_KommentarModuleParamSize)
 
 

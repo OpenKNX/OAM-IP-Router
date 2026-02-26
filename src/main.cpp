dac206a5a4560f0d1398dfb044d9287333e2f720
Ing-Dom <dom@ing-dom.de>
2026-02-26 15:32:23 +0100

    7.4.2-Dev

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,7 @@ bool core1_separate_stack = true;
 
 void setup()
 {
-    const uint8_t firmwareRevision = 1;
+    const uint8_t firmwareRevision = 2;
     openknx.init(firmwareRevision);
 
     openknx.addModule(6, openknxIPRouterModule);
